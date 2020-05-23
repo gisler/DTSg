5a7085b0cb5df7d6043ea29024d23435f974575a
gisler <gisler@users.noreply.github.com>
2020-05-23 09:26:42 +0200

    a whole bunch of minor internal (code) improvements

--- inst/tinytest/test_DTSg.R
+++ inst/tinytest/test_DTSg.R
@@ -13,11 +13,17 @@ expect_identical(
     col2 = c(NA, 6, 10, 14),
     key = "date"
   ),
-  info = "values are aggregated correctly (one function)"
+  info = "values are aggregated correctly (multiple columns and single function)"
 )
 
 expect_identical(
-  DTSg$new(DT1)$aggregate(byYmdH__, list(mean = mean, sum = sum))$values(),
+  DTSg$new(DT1)$aggregate(byYmdH__, mean, cols = "col2", n = TRUE)$values()[[".n"]],
+  c(1L, 2L, 2L, 2L),
+  info = ".n is correct (single column and function)"
+)
+
+expect_identical(
+  DTSg$new(DT1)$aggregate(byYmdH__, list(mean = mean, sum = sum), n = TRUE)$values(),
   data.table(
     date = seq(
       as.POSIXct("2000-10-29 01:00:00", tz = "Europe/Vienna"),
@@ -28,15 +34,16 @@ expect_identical(
     col1.sum  = c( 4, 12, 20, 28),
     col2.mean = c(NA, 6 , 10, 14),
     col2.sum  = c(NA, 12, 20, 28),
+    .n = rep(2L, 4),
     key = "date"
   ),
-  info = "values are aggregated correctly (multiple functions)"
+  info = "values are aggregated correctly and .n is correct (multiple columns and functions)"
 )
 
 expect_identical(
   DTSg$new(DT1)$aggregate(byYmdH__, mean, na.rm = TRUE)$values()[["col2"]],
   c(1, 6, 10, 14),
-  info = '"..." passes on arguments correctly (one function)'
+  info = '"..." passes on arguments correctly (single function)'
 )
 
 expect_identical(
@@ -48,18 +55,6 @@ expect_identical(
 expect_true(
   DTSg$new(DT1)$aggregate(byYmdH__, mean)$aggregated,
   info = '"aggregated" field is set correctly'
-)
-
-expect_identical(
-  DTSg$new(DT1)$aggregate(byYmdH__, mean, cols = "col2", n = TRUE)$values()[[".n"]],
-  c(1L, 2L, 2L, 2L),
-  info = ".n is correct (single column)"
-)
-
-expect_identical(
-  DTSg$new(DT1)$aggregate(byYmdH__, mean, n = TRUE)$values()[[".n"]],
-  rep(2L, 4L),
-  info = ".n is correct (multiple columns)"
 )
 
 #### alter method ####
